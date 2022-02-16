4c47e85452011a1d04793718df86a8235245caa1
Christoph Timm <quaquaro@gmail.com>
2022-02-16 15:00:33 +0100

    Add react-router

---
+++ src/components/Status.js
@@ -0,0 +1,9 @@
+export default function Status() {
+  return (
+    <section>
+      <div>
+        <span>Status: red/green </span>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Status.stories.js
@@ -0,0 +1,13 @@
+import Status from './Status.js';
+
+export default {
+  title: 'Components/Status',
+  component: Status,
+  decorators: [
+    Story => (
+      <div>
+        <Story />
+      </div>
+    ),
+  ],
+};
