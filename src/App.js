799d7fd832656fc5c4107bda485de5290625477e
Christoph Timm <quaquaro@gmail.com>
2022-02-16 15:47:11 +0100

    Add Links and Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { NavLink, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import UserStatusPage from './pages/UserStatusPage.js';
+
 function App() {
-  return <div>Hello App</div>;
+  return (
+    <div>
+      <nav>
+        <StyledLink to="/">User Status</StyledLink>
+        <StyledLink to="/ships">Available ships</StyledLink>
+        <StyledLink to="/market">Marketplace</StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<UserStatusPage />} />
+        <Route path="/ships" element={<div>Available ships</div>} />
+        <Route path="/market" element={<div>Marketplace</div>} />
+      </Routes>
+    </div>
+  );
 }
+
+const StyledLink = styled(NavLink)`
+  margin: 8px;
+  padding: 4px 12px;
+  border-radius: 4px;
+  text-decoration: none;
+  background-color: darkslategray;
+  color: azure;
+
+  &.active {
+    background-color: azure;
+    color: darkslategray;
+    border: 1px solid darkslategray;
+  }
+`;
 
 export default App;
