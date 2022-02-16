099454f1c69885be0ad8650be275c07a0907a849
Christoph Timm <quaquaro@gmail.com>
2022-02-16 15:53:03 +0100

    Add GlobalStyles to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './GlobalStyles.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
